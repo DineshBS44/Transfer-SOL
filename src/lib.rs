340f28863d5b5cd5f6448eabb115433f810581bb
root <root@kboddula1.localdomain>
2021-11-06 23:39:31 +0530

    Added Rust program to transfer lamports from one account to another

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+
+    let account_info_iter = &mut accounts.iter();
+
+    let source_info = next_account_info(account_info_iter)?;
+    let destination_info = next_account_info(account_info_iter)?;
+
+    **source_info.try_borrow_mut_lamports()? -= 100; // 100 lamports are withdrawn from source account
+
+    **destination_info.try_borrow_mut_lamports()? += 100; // 100 lamports are deposited to destination account
+
+    Ok(())
+}
