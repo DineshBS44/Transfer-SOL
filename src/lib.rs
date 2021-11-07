1359b21c44961cfe6228d801fd167ae11ccc7226
root <root@kboddula1.localdomain>
2021-11-07 14:29:44 +0530

    Update contract, configure Cargo.toml and Deployed Smart contract to Devnet

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
+    entrypoint,
     entrypoint::ProgramResult,
     pubkey::Pubkey,
 };
+
+entrypoint!(process_instruction);
 
 pub fn process_instruction(
     _program_id: &Pubkey,
